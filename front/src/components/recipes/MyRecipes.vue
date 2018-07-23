af6672dc6184a809cc422ed05661c1fae0b30577
EtienneR <etienne.rouzeaud@free.fr>
2018-07-23 10:52:13 +0200

    Ajout bouton "Ajouter une recette"

--- front/src/components/recipes/MyRecipes.vue
+++ front/src/components/recipes/MyRecipes.vue
@@ -1,63 +1,70 @@
 <template>
-  <section class="section">
-    <nav class="level">
-      <div class="level-item has-text-centered">
-        <div>
-          <p class="heading">Recettes</p>
-          <p class="title">{{ recipes.length }}</p>
-        </div>
-      </div>
-      <div class="level-item has-text-centered">
-        <div>
-          <p class="heading">Recettes publiées</p>
-          <p class="title">{{ getPublishedRecipes }}</p>
-        </div>
-      </div>
-      <div class="level-item has-text-centered">
-        <div>
-          <p class="heading">Recettes en attente</p>
-          <p class="title">{{ getUnpublishedRecipes }}</p>
-        </div>
-      </div>
-      <div class="level-item has-text-centered">
-        <div>
-          <p class="heading">Like</p>
-          <p class="title">0</p>
-        </div>
-      </div>
-      <div class="level-item has-text-centered">
-        <div>
-          <p class="heading">Fork</p>
-          <p class="title">0</p>
-        </div>
-      </div>
-    </nav>
+	<section class="section">
+		<nav class="level">
+			<div class="level-item has-text-centered">
+				<div>
+					<p class="heading">Recettes</p>
+					<p class="title">{{ recipes.length }}</p>
+				</div>
+			</div>
+			<div class="level-item has-text-centered">
+				<div>
+					<p class="heading">Recettes publiées</p>
+					<p class="title">{{ getPublishedRecipes }}</p>
+				</div>
+			</div>
+			<div class="level-item has-text-centered">
+				<div>
+					<p class="heading">Recettes en attente</p>
+					<p class="title">{{ getUnpublishedRecipes }}</p>
+				</div>
+			</div>
+			<div class="level-item has-text-centered">
+				<div>
+					<p class="heading">Like</p>
+					<p class="title">0</p>
+				</div>
+			</div>
+			<div class="level-item has-text-centered">
+				<div>
+					<p class="heading">Fork</p>
+					<p class="title">0</p>
+				</div>
+			</div>
+		</nav>
 
-    <b-table :data="recipes" hoverable>
-      <template slot-scope="props">
-        <b-table-column label="Titre" field="name">
-          {{ props.row.name }}
-        </b-table-column>
-        <b-table-column label="Publiée" field="published">
-          <span v-if="props.row.published">Publiée</span>
-          <span v-else>Brouillon</span>
-        </b-table-column>
-        <b-table-column label="Date de création" field="created_at">
-          {{ props.row.created_at }}
-        </b-table-column>
-        <b-table-column label="Date de modification" field="updated_at">
-          {{ props.row.updated_at }}
-        </b-table-column>
-        <b-table-column label="Modifier">
-          <router-link class="button is-info" :to="{ name: 'editRecipe', params: { id: props.row.id }}" :title="`Modifier la recette ${props.row.name}`">Modifier</router-link>
-        </b-table-column>
-        <b-table-column label="Supprimer">
-          <a class="button is-danger" @click="remove(props.row.id, props.index)" :title="`Supprimer la recette ${props.row.name}`">Supprimer</a>
-        </b-table-column>
-      </template>
-    </b-table>
+		<router-link class="button is-primary" :to="{ name: 'addRecipe'}">Ajouter une recette</router-link>
 
-  </section>
+		<b-table :data="recipes" hoverable>
+			<template slot-scope="props">
+				<b-table-column label="Titre" field="name">
+					{{ props.row.name }}
+				</b-table-column>
+				<b-table-column label="Publiée" field="published">
+					<span v-if="props.row.published">Publiée</span>
+					<span v-else>Brouillon</span>
+				</b-table-column>
+				<b-table-column label="Date de création" field="created_at">
+					{{ props.row.created_at }}
+				</b-table-column>
+				<b-table-column label="Date de modification" field="updated_at">
+					{{ props.row.updated_at }}
+				</b-table-column>
+				<b-table-column label="Modifier">
+					<router-link class="button is-info"
+						:to="{ name: 'editRecipe', params: { id: props.row.id }}"
+						:title="`Modifier la recette ${props.row.name}`">Modifier
+					</router-link>
+				</b-table-column>
+				<b-table-column label="Supprimer">
+					<a class="button is-danger"
+						@click="remove(props.row.id, props.index)"
+						:title="`Supprimer la recette ${props.row.name}`">Supprimer</a>
+				</b-table-column>
+			</template>
+		</b-table>
+
+	</section>
 </template>
 
 <script>
@@ -67,57 +74,57 @@ import { EventBus } from '@/event-bus.js'
 export default {
 	props: {
 		isConnected: Boolean,
-    userId: Number
+    	userId: Number
 	},
-  data() {
-    return {
-      recipes: [],
-      columns: [
-        {
-          field: 'name',
-          label: 'Titre',
-        },
-        {
-          field: 'published',
-          label: 'Publiée',
-        },
-        {
-          field: 'created_at',
-          label: 'Date de création',
-        },
-        {
-          field: 'updated_at',
-          label: 'Date de modification',
-        }
-      ]
-    }
-  },
-  async created() {
-    this.myRecipes()
-  },
-  computed: {
-    getPublishedRecipes() {
-      return this.recipes.filter(recette => recette.published).length
-    },
-    getUnpublishedRecipes() {
-      return this.recipes.filter(recette => !recette.published).length      
-    }
-  },
-  methods: {
-    async myRecipes() {
-      await api.getRecipesByAuthor(this.userId)
-        .then(res =>  {
-          this.recipes = res.data
-        })
-    },
-    remove(id, index) {
-      return api.removeRecipe(id)
-        .then(() => {
-          this.recipes.splice(index, 1)
-          EventBus.$emit('toast', `Recette ${id} supprimée`)
-        })
-    }
-  }
+  	data() {
+		return {
+			recipes: [],
+			columns: [
+				{
+					field: 'name',
+					label: 'Titre',
+				},
+				{
+					field: 'published',
+					label: 'Publiée',
+				},
+				{
+					field: 'created_at',
+					label: 'Date de création',
+				},
+				{
+					field: 'updated_at',
+					label: 'Date de modification',
+				}
+		]
+		}
+  	},
+	async created() {
+		this.myRecipes()
+	},
+	computed: {
+		getPublishedRecipes() {
+			return this.recipes.filter(recette => recette.published).length
+		},
+		getUnpublishedRecipes() {
+			return this.recipes.filter(recette => !recette.published).length      
+		}
+	},
+	methods: {
+		async myRecipes() {
+			await api.getRecipesByAuthor(this.userId)
+			.then(res =>  {
+				this.recipes = res.data
+			})
+		},
+		remove(id, index) {
+			return api.removeRecipe(id)
+			.then(() => {
+				this.recipes.splice(index, 1)
+				EventBus.$emit('toast', `Recette ${id} supprimée`)
+			})
+		}
+	}
 }
 </script>
 
