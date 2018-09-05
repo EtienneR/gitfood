e8cb4df1363f5ab4498d2a55d2cb7fd3670bdfb2
EtienneR <etienne.rouzeaud@free.fr>
2018-09-05 15:01:34 +0200

    Correction des 2 middlewares

--- api/helpers/middlewares.js
+++ api/helpers/middlewares.js
@@ -10,18 +10,20 @@ function checkIntegerId(req, res, next) {
     if (id) {
         if (!Number.isInteger(parseInt(id))) {
             return res.status(400).json({ message: message.idNotInteger })
+        } else {
+            next()
         }
+    } else {
+        next()
     }
-
-    next()
 }
 
 function checkFields(req, res, next) {
     if (Object.keys(req.body).length === 0) {
         return res.status(400).json({ message: message.emptyFields })
+    } else {
+        next()
     }
-
-    next()
 }
 
 module.exports = {
